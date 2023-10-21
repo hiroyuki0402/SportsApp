4fdc76417f3acdf40c36a1874284aae3a84133b7
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-22 03:28:37 +0900

    カードビューの使用

--- app/src/main/java/com/example/sportsapp/Adapter.java
+++ app/src/main/java/com/example/sportsapp/Adapter.java
@@ -1,4 +1,46 @@
 package com.example.sportsapp;
 
-public class Adapter {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<ViewHolder> {
+
+    public Adapter(ArrayList<SportData> sportDatas) {
+        this.sportDatas = sportDatas;
+    }
+
+    ArrayList<SportData> sportDatas;
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.carditem_layout, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        SportData sportData = sportDatas.get(position);
+
+        int imageResId = holder.imageView.getContext().getResources().getIdentifier(sportData.getImageName(), "drawable", holder.imageView.getContext().getPackageName());
+        holder.imageView.setImageResource(imageResId);
+        holder.textView.setText(sportData.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return sportDatas.size();
+    }
 }
--- app/src/main/java/com/example/sportsapp/MainActivity.java
+++ app/src/main/java/com/example/sportsapp/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.sportsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<SportData> sportDatas;
+
+    /**
+     * 競技の名前リスト
+     */
+    ArrayList<String> names;
+
+    /**
+     * 画像のリソースの名前リスト
+     */
+    ArrayList<String> imageNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycle);
+
+        fruitsItemSetUP();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        Adapter adapter = new Adapter(sportDatas);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    /**
+     * アイテムデータを設定するメソッド
+     */
+    private void fruitsItemSetUP() {
+        imageNames = new ArrayList<>();
+        names = new ArrayList<>();
+        sportDatas = new ArrayList<>();
+
+        imageNames.add("basketball");
+        imageNames.add("football");
+        imageNames.add("ping");
+        imageNames.add("tennis");
+        imageNames.add("volley");
+
+
+        names.add("バスケットボール");
+        names.add("サッカー");
+        names.add("卓球");
+        names.add("テニス");
+        names.add("バレーボール");;
+
+
+        for (int i = 0; i < 5; i++) {
+            SportData sportData = new SportData(imageNames.get(i), names.get(i));
+            sportDatas.add(sportData);
+        }
     }
 }
